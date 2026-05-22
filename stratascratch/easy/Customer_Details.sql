69d3ed2edfc15825efc77a6fa90b56a24ebbbee3
Maksim Chudakov <maksim.chudakoff@gmail.com>
2026-05-22 15:53:22 -0400

    Enhance Customer_Details.sql with problem details
    Added problem statement and comments for clarity.

--- stratascratch/easy/Customer_Details.sql
+++ stratascratch/easy/Customer_Details.sql
@@ -1,3 +1,16 @@
+/*
+Problem: Customer Details
+Platform: StrataScratch
+Problem ID: 9891
+Company: Apple
+Difficulty: Easy
+Link: https://platform.stratascratch.com/coding/9891-customer-details
+*/
+
+-- Find details of each customer regardless of whether they made an order
+-- LEFT JOIN to include customers with no orders (order_details will be NULL)
+-- Sort by first name then order details in ascending order
+
 SELECT first_name, last_name, city, order_details
 FROM customers AS c
 LEFT JOIN orders AS o
