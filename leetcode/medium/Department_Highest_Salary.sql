d3294fb7dcbb2d483d9e4012ff51a46da105d422
Maksim Chudakov <maksim.chudakoff@gmail.com>
2026-05-21 22:20:50 -0400

    Update Department_Highest_Salary.sql

--- leetcode/medium/Department_Highest_Salary.sql
+++ leetcode/medium/Department_Highest_Salary.sql
@@ -9,11 +9,11 @@ Link: https://leetcode.com/problems/department-highest-salary/
 -- Find employees with the highest salary in each department
 
 SELECT Department, Employee, Salary
-FROM(
+FROM (
     SELECT d.name as Department, e.name as Employee , e.salary as Salary,
     RANK() OVER(PARTITION BY e.departmentID ORDER BY e.salary DESC) as rnk
     FROM Employee e 
     JOIN Department d 
         ON e.departmentID=d.id
-)ranked
+) ranked
 where rnk = 1;
