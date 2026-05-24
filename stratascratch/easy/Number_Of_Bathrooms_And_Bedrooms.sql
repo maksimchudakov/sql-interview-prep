f6a71de76a2ad8e793d027e7e25f5191fa35fb0c
Maksim Chudakov <maksim.chudakoff@gmail.com>
2026-05-23 23:48:49 -0400

    Add solution: Number of Bathrooms and Bedrooms (StrataScratch Easy #9622) — Airbnb

---
+++ stratascratch/easy/Number_Of_Bathrooms_And_Bedrooms.sql
@@ -0,0 +1,15 @@
+/*
+Problem: Number Of Bathrooms And Bedrooms
+Platform: StrataScratch
+Problem ID: 9622
+Company: Airbnb
+Difficulty: Easy
+Link: https://platform.stratascratch.com/coding/9622-number-of-bathrooms-and-bedrooms
+*/
+
+SELECT city, property_type,
+    AVG(bathrooms) AS avg_bathrooms,
+    AVG(bedrooms) AS avg_bedrooms
+
+FROM airbnb_search_details
+GROUP BY city, property_type;
