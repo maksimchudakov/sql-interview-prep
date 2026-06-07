71486756d6232b35da919d6799096aae645ad981
Maksim Chudakov <maksim.chudakoff@gmail.com>
2026-06-06 23:53:10 -0400

    Update Department_Highest_Salary.sql

--- leetcode/medium/Department_Highest_Salary.sql
+++ leetcode/medium/Department_Highest_Salary.sql
@@ -10,10 +10,10 @@ Link: https://leetcode.com/problems/department-highest-salary/
 
 SELECT Department, Employee, Salary
 FROM (
-    SELECT d.name as Department, e.name as Employee , e.salary as Salary,
-    RANK() OVER(PARTITION BY e.departmentID ORDER BY e.salary DESC) as rnk
+    SELECT d.name as Department, e.name AS Employee , e.salary AS Salary,
+    RANK() OVER(PARTITION BY e.departmentID ORDER BY e.salary DESC) AS rnk
     FROM Employee e 
     JOIN Department d 
         ON e.departmentID=d.id
 ) ranked
-where rnk = 1;
+WHERE rnk = 1;
