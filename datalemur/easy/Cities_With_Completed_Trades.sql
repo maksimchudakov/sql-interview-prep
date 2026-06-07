11ae211a763aa850afd474c8e1f023f0499713e7
Maksim Chudakov <maksim.chudakoff@gmail.com>
2026-06-06 23:53:35 -0400

    Update Cities_With_Completed_Trades.sql

--- datalemur/easy/Cities_With_Completed_Trades.sql
+++ datalemur/easy/Cities_With_Completed_Trades.sql
@@ -9,7 +9,7 @@ Link: https://datalemur.com/questions/completed-trades
 -- Write a query to retrieve the top three cities that have the highest number
 -- of completed trade orders, ordered in descending order.
 
-SELECT u.city, COUNT(t.order_id) as total_orders
+SELECT u.city, COUNT(t.order_id) AS total_orders
 FROM trades t
 JOIN users u 
   ON t.user_id = u.user_id
